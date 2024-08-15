9c482cb711992929e9fda1413f0f89fe697adc79
Arman Nihal <armansaliknihal@gmail.com>
2024-08-15 18:58:00 +0300

    Setup initial project in Xcode

---
+++ AI Chatbot/AI Chatbot/AI_ChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_ChatbotApp.swift
+//  AI Chatbot
+//
+//  Created by Arman Nihal on 15.8.2024.
+//
+
+import SwiftUI
+
+@main
+struct AI_ChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
